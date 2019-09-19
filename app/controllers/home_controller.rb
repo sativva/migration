f772c1e6917d613fcede22d9deab54dcd06126ef
thomas rondio <sativva@free.fr>
2019-09-19 09:01:26 +0200

    bundle update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,69 @@
   # frozen_string_literal: true
   class HomeController < ShopifyApp::AuthenticatedController
     layout "application"
+    require 'csv'
+
     def index
       @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
       @webhooks = ShopifyAPI::Webhook.find(:all)
     end
+
+    def pages
+      @pages = ShopifyAPI::Page.find(:all, params: { limit: 250 })
+      @pages.each do |page|
+        page_tmp = ShopifyAPI::Page.new(page.attributes)
+        page_tmp.old_id = page_tmp.id
+        page_tmp.shop_id = nil
+        page_tmp.id = nil
+        page_tmp.save
+        sleep(0.5)
+      end
+    end
+
+    def products_from_csv
+      url_csv = '/Users/thomas/Desktop/pb_pro.csv'
+      table = CSV.parse('/Users/thomas/Desktop/pb_pro.csv', headers: true)
+      table[0]
+
+      CSV.open('/Users/thomas/Desktop/pb_products.csv', 'r') do |row|
+        p row
+      end
+
+      filename = '/Users/thomas/Desktop/pppp.csv'
+      file = File.new(filename, 'r')
+      file.each_line("\n") do |row|
+        p row
+        row = row.gsub(/"/,'').split(';')
+        p row
+
+        images = []
+        row[1].split(',').each {|img| images.push({"src": img})}
+        p images
+        pro = ShopifyAPI::Product.new({
+          "title": row[2],
+          "tags": row[4],
+          "price": row[6],
+          "available": row[8],
+          "description": 'Description',
+          "images": images
+        })
+        p pro
+        pro.save
+        sleep(0.5)
+      end
+
+
+
+      @pages = ShopifyAPI::Page.find(:all, params: { limit: 250 })
+      @pages.each do |page|
+        page_tmp = ShopifyAPI::Page.new(page.attributes)
+        page_tmp.old_id = page_tmp.id
+        page_tmp.shop_id = nil
+        page_tmp.id = nil
+        page_tmp.save
+        sleep(0.5)
+      end
+    end
+
   end
 
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,7 +3,7 @@ ShopifyApp.configure do |config|
   config.application_name = ENV["APP_NAME"]
   config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
   config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
-  config.scope = "read_products, write_products"
+  config.scope = "read_content, write_content, read_themes, write_themes, read_products, write_products, read_product_listings, read_customers, write_customers, read_orders, write_orders, read_orders, write_orders, read_draft_orders, write_draft_orders,read_inventory, write_inventory,read_locations, read_fulfillments, write_fulfillments,read_shipping, write_shipping"
                   #"read_content, write_content, read_themes, write_themes, read_products, write_products, read_product_listings, read_customers, write_customers, read_orders, write_orders, read_orders, write_orders, read_all_orders,read_draft_orders, write_draft_orders,read_inventory, write_inventory,read_locations, read_script_tags, write_script_tags, read_fulfillments, write_fulfillments,read_shipping, write_shipping,read_analytics,read_users, write_users,read_checkouts, write_checkouts,read_reports, write_reports, read_price_rules, write_price_rules,read_marketing_events, write_marketing_events,read_resource_feedbacks, write_resource_feedbacks,read_shopify_payments_payouts" # Consult this page for more scope options:
                                   # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = false
